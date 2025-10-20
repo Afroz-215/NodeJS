b813ac87b2a63dcea4c380ffa56bd124e1e7c8e6
Afroz-215 <afrozj215@gmail.com>
2025-10-20 22:30:23 +0530

    modules

---
+++ Lecture 2/index.js
@@ -0,0 +1,10 @@
+// let math = require("./Math")
+
+// console.log(math.addFn(2,4));
+// console.log(math.subFn(6,1));
+
+
+let {addFn, subFn } = require("./Math")
+
+console.log(addFn(2,4));
+console.log(subFn(4,2));
