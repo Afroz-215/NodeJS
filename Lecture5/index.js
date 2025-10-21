eeaf8b9e788e66bb25d4f054c2941260ba3dc04d
Afroz-215 <afrozj215@gmail.com>
2025-10-21 21:53:23 +0530

    Http Server

---
+++ Lecture5/index.js
@@ -0,0 +1,31 @@
+const http = require("http");
+const fs = require("fs");
+
+
+// const myServer =http.createServer((req,res)=>{
+//     // console.log("req received");
+//     console.log(req);
+//     res.end("hello from server");
+// });
+
+const myServer = http.createServer((req, res) => {
+    const log = `${Date.now()}: New Request Received on ${req.url} \n`;
+    fs.appendFile("Log.txt", log, (err, data) => {
+        // res.end("hello from server again");
+        switch (req.url) {
+            case "/":
+                res.end("HomePage");
+                break;
+            case "/about":
+                res.end("I am Afroz");
+                break;
+            default:
+                res.end("404 not found");
+
+        }
+    })
+})
+
+myServer.listen(3000, () => {
+    console.log("Server is running on port no.3000")
+})
