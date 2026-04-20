15daad3ab8312c22999ece93f09e5bd75476ab92
Afroz <afrozj215@gmail.com>
2026-04-20 23:21:21 +0530

    create server

--- Lecture5/index.js
+++ Lecture5/index.js
@@ -1,35 +1,51 @@
-const http = require("http");
-const fs = require("fs");
+// const http = require("http");
+// const fs = require("fs");
+// const url = require("url")
 
+// // const myServer =http.createServer((req,res)=>{
+// //     // console.log("req received");
+// //     console.log(req.--);
+// //     res.end("hello from server");
+// // });
 
-// const myServer =http.createServer((req,res)=>{
-//     // console.log("req received");
-//     console.log(req.--);
-//     res.end("hello from server");
-// });
-
-const myServer = http.createServer((req, res) => {
-    const log = `${Date.now()}: New Request Received on ${req.url} \n`;
+// const myServer = http.createServer((req, res) => {
+//     if(req.url === "/favicon.ico" ) return res.end();
+//     const log = `${Date.now()}: New Request Received on ${req.url} \n`;
+//     const myUrl =url.parse(req.url,true );
+//     console.log(myUrl)
     
-    fs.appendFile("Log.txt", log, (err, data) => {
-        // res.end("hello from server again");
-        switch (req.url) {
-            case "/":
-                res.end("HomePage");
-                break;
-            case "/about":
-                res.end("I am Afroz");
-                break;
-            case "/favicon":
-                res.end("I am Afroz again");
-                break;
-            default:
-                res.end("404 not found");
+//     fs.appendFile("Log.txt", log, (err, data) => {
+//         // res.end("hello from server again");
+//         switch (req.url) {
+//             case "/":
+//                 res.end("HomePage");
+//                 break;
+//             case "/about":
+//                 res.end("I am ${username } ");
+//                 break;
+//             default:
+//                 res.end("404 not found");
 
-        }
-    })
+//         }
+//     })
+// })
+
+// myServer.listen(3000, () => {
+//     console.log("Server is running on port no.3000")
+// })
+
+// let colors =require('colors');
+// console.log(colors.red("hii"))
+
+let http =require('http')
+
+let server = http.createServer((req,res)=>{
+    res.setHeader('Content-Type',"Text/html")
+    res.write('<h2>Hii</h2>')
+    res.end('hello from res.end')
+    process.exit();
 })
 
-myServer.listen(3000, () => {
-    console.log("Server is running on port no.3000")
+server.listen(5000,()=>{
+    console.log("first")
 })
